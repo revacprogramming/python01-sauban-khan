28f9d4a8efaa5a087ff2a10ca1dbdfa47bce314c
sauban-khan <95756161+sauban-khan@users.noreply.github.com>
2022-05-12 10:13:40 +0000

    problem 4

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,24 @@
 # Conditional Execution
+sh=input("enter the hours:")
+sr=input("enter the rate:")
+fh= float(sh)
+fr=float(sr)
 
-hrs = input("Enter hours? ")
+if fh > 40:
+  print("overtime")
+  reg= fr * fh
+  xp= fh * fr
+  print("pay:",xp)
+  otp= (fh-40) * (fr * 0.5)
+  print(reg,otp)
+  xp=reg + otp
+
+else:
+  print("regular")
+  xp=fh*fr
+print("pay:",xp)
+  
+
+    
+  
+
