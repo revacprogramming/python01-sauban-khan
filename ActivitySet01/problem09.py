b5da294d80c8b25b2d05a1baa077362f8491dba2
sauban-khan <95756161+sauban-khan@users.noreply.github.com>
2022-07-26 14:03:14 +0000

    program 9

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,15 @@
 # Lists
+def fuc():
+  filename = open("dataset/romeo.txt")
+  lst=list(filename)
+  l=[]
+  for word in lst:
+    for letter in word:
+      if letter != l:
+        l.append(letter)
+        l.sort()
+        
+  print(l)
+        
 
-filename = "dataset/romeo.txt"
+fuc()
